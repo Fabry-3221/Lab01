373513c0bfe01615ccf5a32ab0584d9410581edd
Fabricio Benitez y Alejandro Rincon <est.fabricio.benitez@unimilitar.edu.co>
2024-08-19 18:26:27 -0500

    Menu de calculadoras

---
+++ src/menugrafi/MenuGrafi.java
@@ -0,0 +1,11 @@
+
+package menugrafi;
+
+public class MenuGrafi {
+
+    public static void main(String[] args) {
+       Principal prin = new Principal();
+    prin.setVisible(true); 
+    }
+    
+}
